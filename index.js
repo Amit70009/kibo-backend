526c13d3d0a0880f19818c7030ff992b0c06c881
Amit Varshney <amit.varshney@kibocommerce.com>
2024-02-02 03:30:27 +0530

    Update API

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ app.use(express.json());
 require('dotenv').config();
 dbconn.databaseConn();
 
+const validApiKeys = [process.env.API_KEY];
+
+const authenticateApiKey = (req, res, next) => {
+  const apiKey = req.headers['api-key'];
+
+  if (!apiKey || !validApiKeys.includes(apiKey)) {
+    return res.status(401).json({ 
+        status: 401,
+        error: 'Unauthorized',
+    message: "You are not authorized to perform this action. Please check with your admin" });
+  }
+
+  next(); // Continue to the next middleware or route
+};
+
+app.use(authenticateApiKey);
 app.use("/api/v1/users", UserLogin);
 app.use("/api/v1/users", RegisterUser);
 app.use("/api/v1/users", Data);
