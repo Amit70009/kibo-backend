8102a2773b5edbc758c5043b7861f38529241410
Amit Varshney <amit.varshney@kibocommerce.com>
2024-04-27 02:41:07 +0530

    Update url

--- Archieved Ticket/Routes.js
+++ Archieved Ticket/Routes.js
@@ -4,7 +4,7 @@ var UserController = require("../Archieved Ticket/Controller");
 var mongoose = require("mongoose");
 var cron = require("node-cron");
 
-userRouter.post("/archieved-tickets", async (req, res) => {
+userRouter.post("/archieved-ticket", async (req, res) => {
     var callLoginMethod = await UserController.archivedTickets();
     res.send({
       status: "Done"
