07553f117d7e25e4c3616a490720b7197f167747
Amit Varshney <amit.varshney@kibocommerce.com>
2024-06-30 13:50:29 +0530

    All Updated

--- Update Ticket/updateRoute.js
+++ Update Ticket/updateRoute.js
@@ -3,10 +3,11 @@ var userRouter = express.Router();
 var UserController = require("../Search Ticket/Controller");
 var mongoose = require("mongoose");
 
-userRouter.put("/update-ticket/", async (req, res) => {
+userRouter.put("/update-ticket", async (req, res) => {
     const data = req.body
     var updateTicket = await UserController.updateTicket(data);
     res.send({
+    //    message: "done"
         status: updateTicket.status,
         message: updateTicket.message,
         // totalTicket: searchByNumber.ticket,
