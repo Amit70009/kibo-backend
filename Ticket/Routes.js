b875cce38caeb08bf6ea24b0fb1007682bcefec8
Amit Varshney <amit.varshney@kibocommerce.com>
2024-07-16 22:22:00 +0530

    Update ticket create response to 10 ticket per page

--- Ticket/Routes.js
+++ Ticket/Routes.js
@@ -6,10 +6,13 @@ var cron = require("node-cron");
 
 userRouter.post("/ticket-create", async (req, res) => {
     var callLoginMethod = await UserController.Ticket();
+    const createdTickets = Array.isArray(callLoginMethod.data.createdTickets) ? callLoginMethod.data.createdTickets.slice(0, 10) : [];
     res.send({
         status: callLoginMethod.status,
         message: callLoginMethod.message,
-        // data: callLoginMethod.data[0]
+        data: {
+            createdTickets: createdTickets
+        }
     })
 })
 
